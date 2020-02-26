85a02492e22eacc016cda8a75aa0d9a794d7b244
Jacob Makarsky <jmm7293@psu.edu>
2020-02-25 19:14:30 -0500

    adding animations using animate.css and custom javascript

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,32 +1,48 @@
 $(document).ready(function()
 {
-	//FADING HELLO LOOP CALLBACK FUNCTION
-	function fader()
+	fader();
+	typeWriter();
+});
+
+//FADING HELLO LOOP CALLBACK FUNCTION
+function fader()
+{
+	$("#helloText").text("Hello");
+	$("#helloText").fadeIn(1500, function()
 	{
-		$("#helloText").text("Hello");
-		$("#helloText").fadeIn(1500, function()
+		$("#helloText").fadeOut(1500, function()
 		{
-			$("#helloText").fadeOut(1500, function()
+			$("#helloText").text("你好");
+			$("#helloText").fadeIn(1500, function()
 			{
-				$("#helloText").text("你好");
-				$("#helloText").fadeIn(1500, function()
+				$("#helloText").fadeOut(1500, function()
 				{
-					$("#helloText").fadeOut(1500, function()
+					$("#helloText").text("Hola");
+					$("#helloText").fadeIn(1500, function()
 					{
-						$("#helloText").text("Hola");
-						$("#helloText").fadeIn(1500, function()
+						$("#helloText").fadeOut(1500, function()
 						{
-							$("#helloText").fadeOut(1500, function()
-							{
-								//fading cycle complete
-								fader();
-							});
+							//fading cycle complete
+							fader();
 						});
 					});
 				});
 			});
 		});
-	}
-	fader();
+	});
+}
 
-});
+var i = 0;
+var txt = 'Jacob Makarsky'; /* The text */
+var speed = 250; /* The speed/duration of the effect in milliseconds */
+
+//typewriter effect
+function typeWriter() 
+{
+  	if (i < txt.length) 
+  	{
+    	document.getElementById("typewrite").innerHTML += txt.charAt(i);
+    	i++;
+    	setTimeout(typeWriter, speed);
+  }
+}
